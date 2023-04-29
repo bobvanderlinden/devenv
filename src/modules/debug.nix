5b42c1eb42a158dcafc13ade05b583155e83a15a
Bob van der Linden <bobvanderlinden@gmail.com>
2023-04-29 13:33:18 +0200

    devenv: debug: set -x in enterShell
    This is a fix to a regression. set -x based on the devenv.debug option
    was part of mkNakedShell. Now that mkNakedShell is gone, this is the
    next best thing.

--- src/modules/debug.nix
+++ src/modules/debug.nix
@@ -1,6 +1,13 @@
-{ lib, ... }:
+{ lib, config, ... }:
 {
   options.devenv = {
     debug = lib.mkEnableOption "debug mode of devenv enterShell script";
+  };
+
+  config = lib.mkIf config.devenv.debug {
+    # Run debug statement before anything else.
+    enterShell = lib.mkOrder 100 ''
+      set -x
+    '';
   };
 }
